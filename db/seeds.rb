f9d088a864fca75039a1a2a1c14e806efac67b0d
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-01 17:27:39 -0600

    Seeds and linked views for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.find_or_create_by(name: 'Jeff', email: 'jeff@email.com')
+User.find_or_create_by(name: 'Amy', email: 'amy@email.com')
