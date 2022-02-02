18deebeb17d736bafbb2855e0cd78e73b85d4918
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-02 10:56:24 -0600

    Facade with methods for info, cast, search results, reviews

---
+++ app/facades/movie_facade.rb
@@ -0,0 +1,27 @@
+class MovieFacade
+  def service
+    MovieService.new
+  end
+
+  def movie_results(query)
+    service.movie_search(query).map do |data|
+      Result.new(data)
+    end
+  end
+
+  def movie_info(movie_id)
+    Movie.new(service.movie_info(movie_id))
+  end
+
+  def movie_cast(movie_id)
+    service.movie_cast(movie_id).map do |data|
+      Cast.new(data)
+    end
+  end
+
+  def movie_reviews(movie_id)
+    service.movie_review(movie_id).map do |data|
+      Review.new(data)
+    end
+  end
+end
