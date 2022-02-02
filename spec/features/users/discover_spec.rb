579e24f4dfa720e7c0f5f098a54dd38e8067301a
Leland Curtis <lelandcurtis88@gmail.com>
2022-02-02 15:37:28 -0700

    Clean up discover specs to remove tests for results page.

--- spec/features/users/discover_spec.rb
+++ spec/features/users/discover_spec.rb
@@ -4,24 +4,14 @@ RSpec.describe 'the user discover page' do
   let!(:user) {User.create!(name: 'Bob', email: 'bob@email.com')}
 
 
-  it "has a form to search by movie title and it takes you to a results page that shows all the relevant movie titles and ratings" do
+  it "has a form to search by movie title that redirects you to a results page" do
     visit "/users/#{user.id}/discover"
 
     VCR.use_cassette('Fight_Club_search') do
-      fill_in :search, with: 'Fight Club'
+      fill_in :q, with: 'Fight Club'
       click_button 'Search'
 
-      #expect(current_path).to eq("/users/#{user.id}/search")
-
-
-      url = 'https://api.themoviedb.org/3/search/movie'
-      response = Faraday.get(url + "?api_key=#{ENV['movie_api_key']}&query=Fight%20Club")
-      data = JSON.parse(response.body, symbolize_name: true)
-      results = data['results']
-      within 'div.movie_0' do
-        expect(page).to have_content("Title: Fight Club")
-        expect(page).to have_content("Vote Average: 8.4")
-      end
+      expect(current_path).to eq("/users/#{user.id}/movies?q=Fight%20Club")
     end
   end
 end
