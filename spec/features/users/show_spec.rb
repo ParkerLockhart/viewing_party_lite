59ccf54c8b7e8c48ef58c30857225ea4a4c9bfdd
Leland Curtis <lelandcurtis88@gmail.com>
2022-02-02 19:03:30 -0700

    Write feature test for host and viewer display.

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -60,5 +60,39 @@ RSpec.describe 'user dashboard' do
         expect(page).to have_content("Time: 6:10 PM")
       end
     end
+
+    it "lists the host" do
+      VCR.use_cassette('create_movie_from_search_dune2') do
+        facade = MovieFacade.new
+        movie_id = facade.movie_results('dune')[0].movie_id
+        movie = facade.movie_info(movie_id)
+        party = create(:party_with_viewers, host: user_1, viewer_count: 4, movie_id: movie_id, start_time: DateTime.new(2022, 02, 02, 18, 10, 0))
+
+        visit user_path(user_1)
+
+        expect(page).to have_content("Host: Jeff")
+      end
+    end
+
+    it "lists all viewers who are invited" do
+      VCR.use_cassette('create_movie_from_search_dune2') do
+        facade = MovieFacade.new
+        movie_id = facade.movie_results('dune')[0].movie_id
+        movie = facade.movie_info(movie_id)
+        viewer_1 = create(:user, name: 'Abby')
+        viewer_2 = create(:user, name: 'Bob')
+        viewer_3 = create(:user, name: 'Charlie')
+        viewers = [viewer_1, viewer_2, viewer_3]
+        party = create(:party_with_viewers, host: user_1, viewers: viewers, movie_id: movie_id, start_time: DateTime.new(2022, 02, 02, 18, 10, 0))
+
+        visit user_path(user_1)
+        within "div.movie_#{movie_id}_viewers" do
+          expect(page).to have_content("Abby")
+          expect(page).to have_content("Bob")
+          expect(page).to have_content("Charlie")
+          expect(page).to_not have_content("Jeff")
+        end
+      end
+    end
   end
 end
