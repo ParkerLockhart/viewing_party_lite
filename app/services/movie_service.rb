5f660f60a1413a3c00db225bb6afb6d4f0500771
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-02 23:06:54 -0600

    Add correct paths to methods

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -8,11 +8,11 @@ class MovieService
   end
 
   def movie_cast(movie_id)
-    get_url("/movie/#{movie_id}/credits?api_key=#{ENV['movie_api_key']}&language=en-US")
+    get_url("/movie/#{movie_id}/credits?api_key=#{ENV['movie_api_key']}&language=en-US")[:cast]
   end
 
   def movie_review(movie_id)
-    get_url("/movie/#{movie_id}/reviews?api_key=#{ENV['movie_api_key']}&language=en-US")
+    get_url("/movie/#{movie_id}/reviews?api_key=#{ENV['movie_api_key']}&language=en-US")[:results]
   end
 
   def movie_info(movie_id)
