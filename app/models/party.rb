fbac40e3139c3ba55e8fe1875e7a2bd6bfb3f5f3
Leland Curtis <lelandcurtis88@gmail.com>
2022-02-02 18:11:11 -0700

    Write party.movie methods.

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,4 +1,17 @@
 class Party < ApplicationRecord
   has_many :party_users
   has_many :users, through: :party_users
+
+  def movie
+    facade = MovieFacade.new
+    facade.movie_info(self.movie_id)
+  end
+
+  def movie_title
+    movie.title
+  end
+
+  def movie_poster
+    movie.poster
+  end
 end
