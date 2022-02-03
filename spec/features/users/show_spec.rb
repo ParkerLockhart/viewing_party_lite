a9952bfb97e2899648da7e3091a08bcbf02525fe
Leland Curtis <lelandcurtis88@gmail.com>
2022-02-02 18:43:01 -0700

    write tests and update view.

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -21,29 +21,43 @@ RSpec.describe 'user dashboard' do
   describe 'viewing party sections' do
 
     it "has a movie image" do
-      VCR.use_cassette('create_movie_from_search_dune') do
+      VCR.use_cassette('create_movie_from_search_dune2') do
         facade = MovieFacade.new
-        movie_id = facade.movie_result('dune')[0].id
-        movie = facace.movie_info(movie_id)
+        movie_id = facade.movie_results('dune')[0].movie_id
+        movie = facade.movie_info(movie_id)
         party = create(:party, host: user_1, movie_id: movie_id)
 
         visit user_path(user_1)
 
-        expect(page).to have_css("img[src=https://image.tmdb.org/t/p/original/d5NXSklXo0qyIYkgV94XAgMIckC.jpg]")
+        expect(page).to have_css("img[src*='https://image.tmdb.org/t/p/original/d5NXSklXo0qyIYkgV94XAgMIckC.jpg']")
       end
     end
 
     it "has a title that links to the movie show page" do
-      VCR.use_cassette('create_movie_from_search_dune') do
+      VCR.use_cassette('create_movie_from_search_dune2') do
         facade = MovieFacade.new
-        movie_id = facade.movie_result('dune')[0].id
-        movie = facace.movie_info(movie_id)
+        movie_id = facade.movie_results('dune')[0].movie_id
+        movie = facade.movie_info(movie_id)
         party = create(:party, host: user_1, movie_id: movie_id)
 
         visit user_path(user_1)
 
-        click_link "Dune"
+        click_link "Title: Dune"
         expect(current_path).to eq("/users/#{user_1.id}/movies/#{movie_id}")
+      end
+    end
+
+    it "has date and time of event" do
+      VCR.use_cassette('create_movie_from_search_dune2') do
+        facade = MovieFacade.new
+        movie_id = facade.movie_results('dune')[0].movie_id
+        movie = facade.movie_info(movie_id)
+        party = create(:party, host: user_1, movie_id: movie_id, start_time: DateTime.new(2022, 02, 02, 6, 10, 0))
+
+        visit user_path(user_1)
+
+        expect(page).to have_content("Date: Feb 2, 2022")
+        expect(page).to have_content("Time: 6:10pm")
       end
     end
   end
