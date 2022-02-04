475123b46fc921fa8f13556d2afb30a1ed52d744
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-03 20:25:28 -0600

    service refactor

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -2,30 +2,29 @@ require 'json'
 
 class MovieService
 
-  def movie_search(query)
+  def self.movie_search(query)
     query.gsub!(/ /, "%20")
-    get_url("/search/movie?api_key=#{ENV['movie_api_key']}&query=#{query}&language=en-US")[:results]
+    get_url("/search/movie?api_key=#{ENV['movie_api_key']}&query=#{query}&language=en-US")
   end
 
-  def movie_cast(movie_id)
-    get_url("/movie/#{movie_id}/credits?api_key=#{ENV['movie_api_key']}&language=en-US")[:cast]
+  def self.movie_cast(movie_id)
+    get_url("/movie/#{movie_id}/credits?api_key=#{ENV['movie_api_key']}&language=en-US")
   end
 
-  def movie_review(movie_id)
-    get_url("/movie/#{movie_id}/reviews?api_key=#{ENV['movie_api_key']}&language=en-US")[:results]
+  def self.movie_review(movie_id)
+    get_url("/movie/#{movie_id}/reviews?api_key=#{ENV['movie_api_key']}&language=en-US")
   end
 
-  def movie_info(movie_id)
+  def self.movie_info(movie_id)
     get_url("/movie/#{movie_id}?api_key=#{ENV['movie_api_key']}&language=en-US")
   end
 
-  def top_rated
-    get_url("/movie/top_rated?api_key=#{ENV['movie_api_key']}&language=en-US")[:results]
+  def self.top_rated
+    get_url("/movie/top_rated?api_key=#{ENV['movie_api_key']}&language=en-US")
   end
 
-  def get_url(url)
+  def self.get_url(url)
     response = Faraday.get("https://api.themoviedb.org/3#{url}")
     parsed = JSON.parse(response.body, symbolize_names: true)
-
   end
 end
