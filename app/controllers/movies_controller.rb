8b117968336a7bc0d9bd8b8c2a250518f3590d5c
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-03 20:25:57 -0600

    Controller fix after refactor

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,26 +1,22 @@
+require 'rails_helper'
+
 class MoviesController < ApplicationController
 
   def index
-    facade = MovieFacade.new
     @user = User.find(params[:user_id])
     if params[:q] == "top rated"
-      @movies = facade.top_movies
+      @movies = MovieFacade.top_movies
       @heading = "Top Rated Movies"
     else
-      @movies = facade.movie_results(params[:q])
+      @movies = MovieFacade.movie_results(params[:q])
       @heading = "Movie results for: #{params[:q]}"
     end
   end
 
   def show
-    facade = MovieFacade.new
     @user = User.find(params[:user_id])
-    @details = facade.movie_info(params[:id])
-    @cast = facade.movie_cast(params[:id])
-    @reviews = facade.movie_reviews(params[:id])
-  end
-
-  def create
-    index
+    @details = MovieFacade.movie_info(params[:id])
+    @cast = MovieFacade.movie_cast(params[:id])
+    @reviews = MovieFacade.movie_reviews(params[:id])
   end
 end
