4617440f0e68127f390ca32162241c76f4d542e9
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-16 15:37:25 -0600

    Add secure password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   has_many :party_users
   has_many :parties, through: :party_users
-  validates :name, :email, presence: true
+  validates :name, :email, :password_digest, presence: true
   validates :email, uniqueness: true
+
+  has_secure_password
 
   def host_parties
     parties.where(party_users: { host: true })
