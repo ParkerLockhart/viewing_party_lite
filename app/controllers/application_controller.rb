ffc72f8bf3afbae5cb52d040cd9753fc5886cf56
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-17 23:00:45 -0600

    Add current_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,24 @@
 class ApplicationController < ActionController::Base
 
+helper_method :current_user
+
 private
   def error_message(errors)
     errors.full_messages.join(', ')
-  end 
+  end
+
+  def current_user
+    User.find(session[:user_id]) if session[:user_id]
+    
+    #memoization code from rails guide
+    # @_current_user ||= session[:current_user_id] &&
+    #   User.find_by(id: session[:current_user_id])
+  end
+
+  def require_user
+    if !current_user
+      flash[:alert] = "You must be logged in to access this page."
+      redirect_to root_path
+    end
+  end
 end
