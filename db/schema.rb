75cafbaf24dc48e63de32653a57f3bbd7d9cea1a
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-17 22:59:11 -0600

    Add user roles column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_16_184744) do
+ActiveRecord::Schema.define(version: 2022_02_18_002147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2022_02_16_184744) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
