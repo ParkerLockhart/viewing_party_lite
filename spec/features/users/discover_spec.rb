c9bf64d0ec79713066071e16a67427c84032f587
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-17 21:16:21 -0600

    Remove user path

--- spec/features/users/discover_spec.rb
+++ spec/features/users/discover_spec.rb
@@ -1,27 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'the user discover page' do
-  let!(:user) {create(:user, name: 'Bob')}
-
 
   it "has a form to search by movie title that redirects you to a results page" do
-    visit "/users/#{user.id}/discover"
+    visit "/discover"
 
     VCR.use_cassette('Fight_Club_search') do
       fill_in :q, with: 'Fight Club'
       click_button 'Search'
 
-      expect(current_path).to eq("/users/#{user.id}/movies")
+      expect(current_path).to eq("/movies")
     end
   end
 
   it "has a button to search for top rated movies that redirects you to a results page" do
-    visit "/users/#{user.id}/discover"
+    visit "/discover"
 
     VCR.use_cassette('top_rated_search') do
       click_button 'Discover Top Rated Movies'
 
-      expect(current_path).to eq("/users/#{user.id}/movies")
+      expect(current_path).to eq("/movies")
     end
   end
 end
